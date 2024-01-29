f1a4839fc039aef47b03352b5bf09afc9a535fd9
Junhson Jean-Baptiste <jjeanba2@binghamton.edu>
2024-01-29 11:09:20 -0500

    updating (waaay after the fact lol)

---
+++ src/day01/solution.rs
@@ -0,0 +1,13 @@
+fn solution(input_file) -> usize {
+
+    // TODO:
+    // - read file and parse sum of each calorie group
+    // - enter sums into max heap (or keep running max?)
+    // - when parsing is done, return largest calorie sum
+
+}
+
+fn main() {
+    let inputFile = "input.txt";
+    println!("The answer is {}", solution(inputFile))
+}
